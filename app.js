6c5cb27522b615ce1a97e959e4ed92526f021a91
Elina <elinasingh1997@gmail.com>
2023-10-12 20:29:43 +0530

    Added a contact form and saved the recieving  message on database

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const mongoose = require('mongoose');
 
 const homeStartingContent = "A lake or an easy weekend is what God wants to decorate it with. Always the author, nor the time of life. Let it be a course of action. Viverra lived in this place. Do not use a microwave oven or a dishwasher. Until the basketball players are not members or members of the arc. Mattis the employee was targeted by the students. The mountains will give birth to a great push, and a ridiculous mouse will be born in the ultricia of life. I'm trying to find a way to get rid of the poison bed. The author of the life of Ultrices advocates football as a bed of alcohol to drink. Odio euismod lacinia at quis risus sed vulputate odio ut The course of the real estate agent was aimed at the students.";
 const aboutContent = "It is said that he lived in this street, and that it was the vestibule of the children. It is said that the vestibulum of the rhoncus is a corporal punishment. It is not just a fancy vestibule. The street was said to be clear of arrows. But you need a smile at the price of what vulputate dignissim has suspended. Let's get into some salad. Always smile in the hendrerit gravida rutrum every one not the land of the orci. He likes the massa vitae tortor sauce lacinia quis or eros. For the earth is the element of the arrows of life. Mauris ultrices eros in the course of the university mass tincidunt dui.";
-const contactContent = "Scelerisque eleifend until the price vulputate sapien. Roncus urn is neither cartoon nor basketball. Let us live with the bow of God, let us drink the bow of the cat. It will be followed by the education of the family of the sad family. Risus viverra adipiscing at in the whole land of feugiat. It is not wise to drink the bow of life that is expected from arrows. The result is sometimes a lot of real estate. But now the target is the propaganda lake. Sometimes putting the internet itself is a pain in the ass. For the whole element of the pillow is neither. The pregnant woman was told that there was no clinical placement. Mauris is in some sort of environment as a disease. To put a twister and to always cartoon for free.";
 
 const app = express();
 app.set('view engine', 'ejs');
@@ -91,7 +90,33 @@ app.get("/about", function(req, res){
 });
 
 app.get("/contact", function(req, res){
-  res.render("contact", {contactContent: contactContent});
+  res.render("contact");
+});
+
+app.post('/contact', (req, res) => {
+  const { yourName, yourEmail, yourMessage } = req.body;
+
+  const messageSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    message: String
+  });
+  const Message = mongoose.model('Message', messageSchema);
+
+  const newMessage = new Message({
+    name: yourName,
+    email: yourEmail,
+    message: yourMessage
+  });
+
+  newMessage.save()
+  .then(() => {
+    res.redirect('/');
+  })
+  .catch(err => {
+    console.error(err);
+    res.send("There was an error saving your message. Please try again.");
+  });
 });
 
 app.listen(3000, function() {
